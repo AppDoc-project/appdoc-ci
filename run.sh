1e164ef42d182dcc765b2e3462b3d1f4c763fb96
fearlessdeveloper <1dilumn0@gmail.com>
2023-11-17 21:58:21 +0900

    [FEAT] 배포용 도커파일 작성

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# MySQL 서비스 실행
+docker-compose up -d mysql
+
+sleep 1
+
+# Authentication 서비스 실행
+docker-compose up -d authentication
+
+# Authentication 서비스가 완료될 때까지 대기
+
+sleep 5
+
+# Community 서비스 실행
+docker-compose up -d community
+
+# Community 서비스가 완료될 때까지 대기
+
+sleep 5
+
+# Nginx 서비스 실행
+docker-compose up -d nginx
