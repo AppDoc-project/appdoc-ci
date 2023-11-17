9f1d3418a96c6030766855795bb001237cb3d9cc
fearlessdeveloper <1dilumn0@gmail.com>
2023-11-17 22:10:54 +0900

    [FEAT] 배포용 도커파일 작성

---
+++ down.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONTAINERS=("app_authentication" "app_nginx" "app_community" "app_mysql")
+
+
+for container_name in "${CONTAINERS[@]}"
+do
+    echo "Stopping container: ${container_name}"
+    docker stop "${container_name}"
+
+    echo "Removing container: ${container_name}"
+    docker rm "${container_name}"
+done
