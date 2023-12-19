f6f024328aaddb7e8a47a9bf02501d6f586ce705
fearlessdeveloper <1dilumn0@gmail.com>
2023-12-19 16:18:40 +0900

    [FIX] run.sh 수정

--- run.sh
+++ run.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 
 # Docker Compose 파일을 명시적으로 지정하여 실행
-docker-compose -f docker-compose.arm.yml up -d mysql
-
-# MySQL 서비스가 완료될 때까지 대기
-sleep 5
-
-# Docker Compose 파일을 명시적으로 지정하여 실행
-docker-compose -f docker-compose.arm.yml up -d authentication
-
-# Authentication 서비스가 완료될 때까지 대기
-sleep 5
-
-# Docker Compose 파일을 명시적으로 지정하여 실행
-docker-compose -f docker-compose.arm.yml up -d community
-
-# Community 서비스가 완료될 때까지 대기
-sleep 5
-
-# Docker Compose 파일을 명시적으로 지정하여 실행
-docker-compose -f docker-compose.arm.yml up -d nginx
+docker-compose -f docker-compose.arm.yml up
