55ade04db2316e483d0ea255882a718b40b15a51
fearlessdeveloper <1dilumn0@gmail.com>
2023-12-30 16:58:33 +0900

    [HOTFIX] 윈도우용 도커 파일 수정

--- run.ps1
+++ run.ps1
@@ -1,19 +1,4 @@
-# MySQL 서비스 실행
-docker-compose up -d mysql
+#!/bin/bash
 
-Start-Sleep -Seconds 1
-
-# Authentication 서비스 실행
-docker-compose up -d authentication
-
-# Authentication 서비스가 완료될 때까지 대기
-Start-Sleep -Seconds 5
-
-# Community 서비스 실행
-docker-compose up -d community
-
-# Community 서비스가 완료될 때까지 대기
-Start-Sleep -Seconds 5
-
-# Nginx 서비스 실행
-docker-compose up -d nginx
+# Docker Compose 파일을 명시적으로 지정하여 실행
+docker-compose up
