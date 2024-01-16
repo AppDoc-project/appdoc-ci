12b234790520fc62aadf3be90334689ba0a711b9
fearlessdeveloper <1dilumn0@gmail.com>
2024-01-17 00:20:10 +0900

    [HOTFIX] 윈도우용 도커파일 수정

--- run.ps1
+++ run.ps1
@@ -19,9 +19,9 @@ docker-compose -f docker-compose.yml up -d authentication
 docker-compose -f docker-compose.yml up -d community
 
 # Chat 서비스 백그라운드로 실행
-docker-compose -f docker-compose.arm.yml up -d socket
+docker-compose -f docker-compose.yml up -d socket
 
 # Authentication 및 Community 서비스 로그 합치기
 docker-compose -f docker-compose.yml logs -f authentication community
 
-docker-compose -f docker-compose.arm.yml logs -f socket
+docker-compose -f docker-compose.yml logs -f socket
