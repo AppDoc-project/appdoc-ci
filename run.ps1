fb2e85489af785b792eaae21ecae5db2e4408d5d
fearlessdeveloper <1dilumn0@gmail.com>
2024-01-19 19:54:24 +0900

    [HOTFIX] 스크립트 수정

--- run.ps1
+++ run.ps1
@@ -1,28 +1,32 @@
 # MySQL 서비스 백그라운드로 실행
-docker-compose up -d mysql
+# docker-compose -f docker-compose.arm.yml up -d mongodb
+
+docker-compose -f docker-compose.yml up -d mysql
+
+docker-compose -f docker-compose.yml up -d mongodb
 
 # 10초 대기
-Start-Sleep -Seconds 10
-Write-Output "mysql 실행완료"
+Start-Sleep -Seconds 15
+Write-Output "mysql mongodb 실행완료"
 
 # Nginx 서비스 백그라운드로 실행
-docker-compose up -d nginx
 
-docker-compose up -d redis
+docker-compose -f docker-compose.yml up -d nginx
 
-docker-compose up -d mongodb
+docker-compose -f docker-compose.yml up -d redis
+
 
 Start-Sleep -Seconds 5
 Write-Output "서버 가동"
 
 # Authentication 서비스 백그라운드로 실행
-docker-compose up -d authentication
+docker-compose -f docker-compose.yml up -d socket
 
-# Community 서비스 백그라운드로 실행
-docker-compose up -d community
+docker-compose -f docker-compose.yml up -d authentication
 
-# Chat 서비스 백그라운드로 실행
-docker-compose up -d socket
+docker-compose -f docker-compose.yml up -d community
+
+
 
 # Authentication 및 Community 서비스 로그 합치기
-docker-compose logs -f authentication, community, socket
+docker-compose -f docker-compose.yml logs -f authentication community socket
