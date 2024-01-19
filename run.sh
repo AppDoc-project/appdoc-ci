88dda947e3255f6709d1dba21ff53b7011065f21
fearlessdeveloper <1dilumn0@gmail.com>
2024-01-19 17:41:49 +0900

    [HOTFIX] 스크립트 수정

--- run.sh
+++ run.sh
@@ -14,15 +14,13 @@ sleep 10
 echo "mysql mongodb redis nginx 실행완료"
 
 
-
-
-
-
-sleep 5
-echo "서버 가동"
-
 # Authentication 서비스 백그라운드로 실행
 docker-compose -f docker-compose.arm.yml up -d authentication
+
+sleep 5
+echo "인증 서버 가동"
+
+
 
 # Community 서비스 백그라운드로 실행
 docker-compose -f docker-compose.arm.yml up -d community
