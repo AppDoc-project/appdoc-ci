d6db13c9708d37fc045ed87bef3a1cca8428d2db
fearlessdeveloper <1dilumn0@gmail.com>
2024-01-27 00:13:14 +0900

    [FEAT] 운영 환경 도커파일 생성

---
+++ dev.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# MySQL 서비스 백그라운드로 실행
+docker-compose -f docker-compose.dev.yml up -d mysql
+
+# 10초 대기
+sleep 10
+echo "mysql 실행완료"
+
+# Nginx 서비스 백그라운드로 실행
+docker-compose -f docker-compose.dev.yml up -d nginx
+
+docker-compose -f docker-compose.dev.yml up -d redis
+
+docker-compose -f docker-compose.dev.yml up -d mongodb
+
+sleep 5
+echo "서버 가동"
+
+# Authentication 서비스 백그라운드로 실행
+docker-compose -f docker-compose.dev.yml up -d authentication
+
+# Community 서비스 백그라운드로 실행
+docker-compose -f docker-compose.dev.yml up -d community
+exi
+# Chat 서비스 백그라운드로 실행
+docker-compose -f docker-compose.dev.yml up -d socket
+
+# Authentication 및 Community 서비스 로그 합치기
+docker-compose -f docker-compose.dev.yml logs -f authentication community socket
+
+
+
