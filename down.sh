9de64dd5e1158a4bbc0f3b2ff84accf96d31478f
fearlessdeveloper <1dilumn0@gmail.com>
2024-01-27 01:06:07 +0900

    [HOTFIX] 스크립트 수정

---
+++ down.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONTAINERS=("app_authentication" "app_nginx" "app_community" "app_mysql" "app_redis" "app_socket" "app_mongodb")
+
+
+for container_name in "${CONTAINERS[@]}"
+do
+    echo "Stopping container: ${container_name}"
+    docker stop "${container_name}"
+
+    echo "Removing container: ${container_name}"
+    docker rm "${container_name}"
+done
+
+
+
+
